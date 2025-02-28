e855a38013cb374f7103ff27646732ea984aa00a
kaniX <jeremy.schneider.official@proton.com>
2025-02-28 09:46:42 +0100

    add fontawesome self hosted and check if works | have issue that WSL does not trigger hot module reload

--- 0xkanix_website/src/main.ts
+++ 0xkanix_website/src/main.ts
@@ -1,14 +1,31 @@
 import './assets/main.css'
+import 'vuetify/styles'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import { aliases, fa } from 'vuetify/iconsets/fa'
 
 import App from './App.vue'
 import router from './router'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    }
+  }
+})
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
